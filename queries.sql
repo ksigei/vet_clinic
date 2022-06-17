2a7dc28c9309fa4891c94b363d746a7349e166ca
sigeikiprono4@gmail.com <sigeikiprono4@gmail.com>
2022-06-17 17:13:21 +0300

    Query multiple tables

--- queries.sql
+++ queries.sql
@@ -30,7 +30,6 @@ SELECT * from animals
 BEGIN;
 -- Delete all animals born after Jan 1st, 2022.
 DELETE FROM animals WHERE date_of_birth > '2022/01/01';
-
 -- Create a savepoint for the transaction.
 SAVEPOINT sp1;
 UPDATE animals SET weight_kg = weight_kg*-1;
@@ -43,3 +42,11 @@ SELECT AVG(weight_kg) FROM animals;
 SELECT name FROM animals WHERE escape_attempts=(SELECT MAX(escape_attempts) FROM animals);
 SELECT MIN(weight_kg), MAX(weight_kg) FROM animals;
 SELECT AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990/01/01' AND '2000/12/31';
+-- query multiple tables
+SELECT name FROM animals as a INNER JOIN owners as o ON a.owner_id = o.id WHERE o.full_name ='Melody Pond';
+SELECT a.name FROM animals as a INNER JOIN species as s ON species_id = s.id WHERE s.name ='Pokemon';
+SELECT o.full_name, a.name FROM owners as o FULL JOIN animals as a ON o.id = a.owner_id;
+SELECT s.name,COUNT(*) FROM animals as a INNER JOIN species as s ON a.species_id = s.id GROUP BY s.name;
+SELECT a.name FROM animals as a INNER JOIN owners as o ON a.species_id = o.id WHERE o.full_name='Jennifer Orwell';
+SELECT a.name FROM animals as a INNER JOIN owners as o ON a.owner_id = o.id WHERE o.full_name='Dean Winchester' AND a.escape_attempts=0;
+SELECT o.full_name, COUNT(*) as c FROM owners as o INNER JOIN animals as a ON a.owner_id = o.id GROUP BY o.full_name ORDER by c DESC LIMIT 1;
--- schema.sql
+++ schema.sql
@@ -4,3 +4,11 @@ CREATE TABLE animals(id int, name VARCHAR(100), date_of_birth date,escape_attemp
 
 -- Add column to animals table
 ALTER TABLE animals ADD species VARCHAR(100);
+-- query mulitple tables
+CREATE TABLE owners(id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,full_name VARCHAR NOT NULL,age INT NOT NULL);
+CREATE TABLE species(id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,name VARCHAR NOT NULL);
+ALTER TABLE animals DROP species;
+ALTER TABLE animals ADD species_id INT;
+ALTER TABLE animals ADD FOREIGN KEY(species_id) REFERENCES species(id);
+ALTER TABLE animals ADD owner_id INT;
+ALTER TABLE animals ADD FOREIGN KEY(owner_id) REFERENCES owners(id);
